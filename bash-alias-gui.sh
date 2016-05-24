224d7f80bbe530653a27fa070fe14806c3161f6d
Sy Le <lenguyensy@gmail.com>
2016-05-24 12:43:53 -0700

    Update bash-alias-gui.sh

--- bash-alias-gui.sh
+++ bash-alias-gui.sh
@@ -1,2 +1,9 @@
 #For GUI Distro
 #	alias subl=/opt/sublime_text/sublime_text
+
+#mac specific
+	#screensaver
+	function s(){
+		echo "starting screensaver for mac";
+		open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
+	}
