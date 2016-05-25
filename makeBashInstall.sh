7c023ca6df636e604637146fa7bc8655e31c999e
Sy Le <lenguyensy@gmail.com>
2016-05-25 08:22:28 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -5,5 +5,7 @@ else
   echo "Setting stuffs up for Linux";
   BASH_PATH=~/.bash_profile;
 fi
+
+echo "Setting up in bash folder: $BASH_PATH"
 echo '#syle bash' >> $BASH_PATH;
 echo '. ~/.bash_syle' >> $BASH_PATH;
