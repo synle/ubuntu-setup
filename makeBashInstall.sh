8a624cbcd74f4b495f7b57650db11396ce3ef7ea
Sy Le <lenguyensy@gmail.com>
2016-05-25 08:21:02 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -1,4 +1,9 @@
-BASH_PATH=~/.bashrc;
-#BASH_PATH=~/.bash_profile;
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Setting stuffs up for Mac";
+  BASH_PATH=~/.bashrc;
+else then
+  echo "Setting stuffs up for Linux";
+  BASH_PATH=~/.bash_profile;
+fi
 echo '#syle bash' >> $BASH_PATH;
 echo '. ~/.bash_syle' >> $BASH_PATH;
