3964d2f189bc001dfead0b852e07f2b01d04a4bf
Sy Le <lenguyensy@gmail.com>
2016-08-31 09:38:35 -0700

    Update bash-alias-gui.sh

--- bash-alias-gui.sh
+++ bash-alias-gui.sh
@@ -2,12 +2,18 @@
 #	alias subl=/opt/sublime_text/sublime_text
 
 #mac specific
+if [ "$(uname)" == "Darwin" ]; then
 	#screensaver
 	function s(){
 		echo "starting screensaver for mac";
 		open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
 	}
-
-
+	
+	
 	#sublime text alias
 	alias subl='open -a "Sublime Text"'
+	
+	
+	#default java path
+	export JAVA_HOME="$(/usr/libexec/java_home)"
+fi
