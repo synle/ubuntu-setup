ed0c00565b9f3130075d1dc355ad1fc211d3ee02
Sy Le <lenguyensy@gmail.com>
2016-09-05 12:15:05 -0700

    Update makeSetup.sh

--- makeSetup.sh
+++ makeSetup.sh
@@ -1,2 +1,4 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash;
 
+
+npm i -g  grunt-cli grunt-init bower gulp browserify webpack eslint typings;
