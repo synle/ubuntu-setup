4770771fd4b6b4832bcab4ccba66742cf3c3a1eb
Sy Le <lenguyensy@gmail.com>
2016-09-05 12:16:33 -0700

    Update makeSetup.sh

--- makeSetup.sh
+++ makeSetup.sh
@@ -1,4 +1,5 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash;
+sudo apt-get install -y curl build-essential openjdk-7-jdk python-dev python-software-properties software-properties-common g++ python supervisor automake gnuplot unzip vim ant gradle maven git maven make mysql-client;
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash;
 
 npm i -g  grunt-cli grunt-init bower gulp browserify webpack eslint typings;
