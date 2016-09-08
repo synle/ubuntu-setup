1cdc1b64f7a62c64cd79a006fb244fc88cffc577
Sy Le <lenguyensy@gmail.com>
2016-09-08 16:59:45 -0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "q$": true,
+        "$": true,
+        "module": true,
+        "angular": true
+    }
+};
