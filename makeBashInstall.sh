352f5dfe03818257955b7f593aebb76b8588759a
Sy Le <lenguyensy@gmail.com>
2016-10-11 20:45:45 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -14,9 +14,3 @@ echo '. ~/.bash_syle' >> $BASH_PATH;
 
 echo "Set up git stuffs"
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/vagrant-box/.gitconfig >> ~/.gitconfig
-
-
-echo "Set up vim"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/vim/.vimrc > ~/.vimrc;
-vim +BundleInstall +qall
