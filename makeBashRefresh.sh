916bd758e5e1103f124572623265923830031f2e
Sy Le <lenguyensy@gmail.com>
2016-10-11 20:47:21 -0700

    Update makeBashRefresh.sh

--- makeBashRefresh.sh
+++ makeBashRefresh.sh
@@ -32,15 +32,53 @@ echo 'Setting up in ' + $TEMP_BASH_SYLE
   #misc
   #eslint config
   curl -so-  https://raw.githubusercontent.com/synle/ubuntu-setup/master/.eslintrc > ~/.eslintrc
-  
-  #vim stuffs
-  echo "Set up vim"
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-  curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/vim/.vimrc > ~/.vimrc;
-  vim +BundleInstall +qall
 
 #copy it over
 mv $TEMP_BASH_SYLE $BASH_SYLE
 
 #rerun the source
 . $BASH_SYLE
+
+
+
+#extra stuffs
+#vim stuffs
+echo "Set up vim"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/vim/.vimrc > ~/.vimrc;
+vim +BundleInstall +qall
+
+
+
+#awesome git commands
+#config
+git config --global user.name "Sy Le"
+git config --global core.autocrlf input
+git config --global core.editor vim
+git config --global push.default simple
+git config --global merge.tool vimdiff
+git config --global diff.tool vimdiff
+
+#git alias
+git config --global alias.del-merged-branches "!git branch --merged | grep -v '*' | xargs git branch -d"
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.co 'checkout'
+git config --global alias.coo 'checkout --orphan' # new branch no history
+git config --global alias.cp 'cherry-pick'
+git config --global alias.cm 'commit'
+git config --global alias.del 'branch -D'
+git config --global alias.br 'branch -v'
+git config --global alias.b 'branch'
+git config --global alias.branch 'branch -a'
+git config --global alias.p 'push'
+git config --global alias.logs 'log --oneline --decorate'
+git config --global alias.fap 'fetch --all --prune'
+git config --global alias.st 'status -sb'
+git config --global alias.s 'status -sb'
+git config --global alias.amend 'commit --amend'
+git config --global alias.nuke '!git reset --hard && git clean -dfx && git gc && git prune'
+git config --global alias.push-force 'push --force-with-lease'
+git config --global alias.commend 'commit --amend --no-edit'
+git config --global alias.it '!git init && git commit -m “root” --allow-empty'
+git config --global alias.stash 'stash --all'
+#end git
