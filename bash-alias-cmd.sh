dd06a6d68899872dec172a70dfa849f2b41a065b
Sy Le <lenguyensy@gmail.com>
2016-10-14 12:05:47 -0700

    Update bash-alias-cmd.sh

--- bash-alias-cmd.sh
+++ bash-alias-cmd.sh
@@ -114,23 +114,10 @@
 	
 	# cdf - cd into the directory of the selected file
 	function fd() {
-	   local file
-	   local dir
-	   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
-	}
-	
-	function fdr() {
-	  local declare dirs=()
-	  get_parent_dirs() {
-	    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
-	    if [[ "${1}" == '/' ]]; then
-	      for _dir in "${dirs[@]}"; do echo $_dir; done
-	    else
-	      get_parent_dirs $(dirname "$1")
-	    fi
-	  }
-	  local DIR=$(get_parent_dirs $(realpath "${1:-$(pwd)}") | fzf-tmux --tac)
-	  cd "$DIR"
+	  local dir
+	  dir=$(find ${1:-.} -path '*/\.*' -prune \
+			  -o -type d -print 2> /dev/null | fzf +m --preview="ls -la {}") &&
+	  cd "$dir"
 	}
 
 	
