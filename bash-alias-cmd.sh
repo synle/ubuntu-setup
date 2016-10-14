85b342a3d53556586ff3f7841d9c169492be0d6a
Sy Le <lenguyensy@gmail.com>
2016-10-14 11:33:26 -0700

    Update bash-alias-cmd.sh

--- bash-alias-cmd.sh
+++ bash-alias-cmd.sh
@@ -162,16 +162,12 @@ f		#     --line-number \
 # 		ag --nobreak --nonumbers --noheading . | fzf
 	}
 	
-	function runcmd (){
-		perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>' ; 
-	}
+	runcmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>' ; }
+	writecmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>); $_ }' ; }
 	
 	function fh() {
 	  ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
 	}
-	
-	# fhe - repeat history edit
-	function writecmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>); $_ }' ; }
 
 	function fhe() {
 	  ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
