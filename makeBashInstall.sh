45dc5f53d9e70524b558f1e33c8ea2d4b7cd2889
Sy Le <lenguyensy@gmail.com>
2016-10-15 13:11:43 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -4,10 +4,6 @@ BASH_PATH=~/.bashrc;
 if [ "$(uname)" == "Darwin" ]; then
   echo "Doing Mac Specific Setup:";
   BASH_PATH=~/.bash_profile;
-  
-  #mac specific
-  curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/mac/mac.setup.sh | bash -;
-  
   brew install fzf jq;
 fi
 
@@ -18,7 +14,3 @@ echo '. ~/.bash_syle' >> $BASH_PATH;
 
 echo "Set up git stuffs"
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/vagrant-box/.gitconfig >> ~/.gitconfig
-
-
-echo "set up vim vundle"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
