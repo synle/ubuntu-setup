3d66c76ace0568e960da5d363596b04f7931e620
Sy Le <lenguyensy@gmail.com>
2016-10-15 18:47:54 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/env bash
 BASH_PATH=~/.bashrc;
 
-if [ "$(uname)" == "Darwin" ]; then
-  echo "Getting Things Ready for Mac:";
-  BASH_PATH=~/.bash_profile;
+#is ubuntu
+is_ubuntu=0
+apt-get -v &> /dev/null && is_ubuntu=1
+if [ $is_ubuntu == "1" ]; then
+  echo "Ubuntu apt-get install...";
+  sudo apt-get install -y curl build-essential openjdk-7-jdk python-dev python-software-properties software-properties-common g++ python supervisor automake gnuplot unzip vim ant gradle maven git maven make mysql-client;
 fi
 
+if [ "$(uname)" == "Darwin" ]; then
+  BASH_PATH=~/.bash_profile;
+  
+  echo "install brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+echo "Install nvm"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash;
+
+#source the bash profile
 echo "Setting up in bash folder: $BASH_PATH"
 echo '#syle bash' >> $BASH_PATH;
 echo '. ~/.bash_syle' >> $BASH_PATH;
 
-echo "Set up git stuffs"
-curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/.gitconfig >> ~/.gitconfig
+
+echo "Re-source Bash Profile for nvm binary"
+. ${BASH_PATH}
+
+
+#node --version
+#v0.12.15
+echo "Install node@v0.12.15";
+nvm install v0.12.15;
+nvm alias default v0.12.15;
+ 
+#npm --version
+#2.15.1
+echo "Install npm@2.15.1";
+npm install npm@2.15.1 -g;
+
+#download node npm deps
+echo "Install Global Node Packages";
+npm i -g  grunt-cli grunt-init bower gulp browserify webpack eslint typings;
