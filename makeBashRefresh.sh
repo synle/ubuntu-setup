e60c2b9d4b98184555650489fbc84a35d00c4faa
Sy Le <lenguyensy@gmail.com>
2016-10-15 17:47:48 -0700

    Update makeBashRefresh.sh

--- makeBashRefresh.sh
+++ makeBashRefresh.sh
@@ -21,17 +21,17 @@ echo "Set up temp bash file: $TEMP_BASH_SYLE"
     echo "Set up Mac OSX (Darwin) specifics..."
     
     # mac alias
-    echo "Mac OSX Aliases"
+    echo " > Mac OSX Aliases"
     curl -so- -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/synle/ubuntu-setup/master/bash-alias-gui.osx.sh >> $TEMP_BASH_SYLE
     
     # mac sublime
-    echo "Mac OSX Sublime Symlink"
+    echo " > Mac OSX Sublime Symlink"
     rm -f /usr/local/bin/subl;
     ln -sf /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl;
     chmod +x /usr/local/bin/subl;
 
     # mac options 
-    echo "Mac OSX Options"
+    echo " > Mac OSX Options"
     curl -so- -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/synle/ubuntu-setup/master/mac/mac.setup.sh | bash -;
   fi
 
