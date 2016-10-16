8110f1da38b2adcbfcffc8e1f14fcbc20595fc9c
Sy Le <lenguyensy@gmail.com>
2016-10-15 18:03:47 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -2,21 +2,13 @@
 BASH_PATH=~/.bashrc;
 
 if [ "$(uname)" == "Darwin" ]; then
-  echo "Doing Mac Specific Setup:";
+  echo "Getting Things Ready for Mac:";
   BASH_PATH=~/.bash_profile;
-  brew install fzf jq;
 fi
-
-# else 
-# fzf
-# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-# ~/.fzf/install
-
 
 echo "Setting up in bash folder: $BASH_PATH"
 echo '#syle bash' >> $BASH_PATH;
 echo '. ~/.bash_syle' >> $BASH_PATH;
-
 
 echo "Set up git stuffs"
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/.gitconfig >> ~/.gitconfig
