e70cfb1e592bf1fc8f2103bafca29348c904a363
Sy Le <lenguyensy@gmail.com>
2016-10-15 21:20:07 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -5,6 +5,12 @@ BASH_PATH=~/.bashrc;
 is_ubuntu=0
 apt-get -v &> /dev/null && is_ubuntu=1
 if [ $is_ubuntu == "1" ]; then
+  #source the bash profile
+  echo "Setting up in bash folder: $BASH_PATH"
+  echo '#syle bash' >> $BASH_PATH;
+  echo '. ~/.bash_syle' >> $BASH_PATH;
+
+
   echo "Ubuntu apt-get install...";
   sudo apt-get update -y;
   sudo apt-get install -y curl build-essential openjdk-7-jdk python-dev python-software-properties software-properties-common g++ python supervisor automake gnuplot unzip vim ant gradle maven git maven make mysql-client;
@@ -12,9 +18,15 @@ fi
 if [ "$(uname)" == "Darwin" ]; then
   BASH_PATH=~/.bash_profile;
   
+  #source the bash profile
+  echo "Setting up in bash folder: $BASH_PATH"
+  echo '#syle bash' >> $BASH_PATH;
+  echo '. ~/.bash_syle' >> $BASH_PATH;
+  
   echo "Install Homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   
+  echo "Brew install stuffs"
   brew install jq fzf;
 fi
 
@@ -23,24 +35,13 @@ echo "Install nvm@v0.32.0"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
 
-#source the bash profile
-echo "Setting up in bash folder: $BASH_PATH"
-echo '#syle bash' >> $BASH_PATH;
-echo '. ~/.bash_syle' >> $BASH_PATH;
-
-
 echo "Re-source Bash Profile for nvm binary"
 . ${BASH_PATH}
 
-
-#node --version
-#v0.12.15
 echo "Install node@v0.12.15";
 nvm install v0.12.15;
 nvm alias default v0.12.15;
  
-#npm --version
-#2.15.1
 echo "Install npm@2.15.1";
 npm install npm@2.15.1 -g;
 
