9d3b77cc7b2db31d3e938b0a28845a995bf84d6e
Sy Le <lenguyensy@gmail.com>
2016-10-15 18:33:24 -0700

    Update makeSetup.sh

--- makeSetup.sh
+++ makeSetup.sh
@@ -6,10 +6,11 @@ if [ $is_ubuntu == "1" ]; then
 fi
 
 
-echo "Install NVM"
+echo "Install nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash;
 
 #source the bash profile
+echo "Resource Bash Profile for nvm binary"
 BASH_PROFILE_HOME_DIRECTORY=~/.bashrc
 if [ "$(uname)" == "Darwin" ]; then
 	export BASH_PROFILE_HOME_DIRECTORY=~/.bash_profile
@@ -20,14 +21,15 @@ fi
 
 #node --version
 #v0.12.15
-echo "Install node@v0.12.15"
-nvm install v0.12.15
-nvm alias default v0.12.15
+echo "Install node@v0.12.15";
+nvm install v0.12.15;
+nvm alias default v0.12.15;
  
 #npm --version
-echo "Install npm@2.15.1"
 #2.15.1
+echo "Install npm@2.15.1";
+npm install npm@2.15.1 -g;
 
 #download node npm deps
-echo "Install Global Node Packages"
+echo "Install Global Node Packages";
 npm i -g  grunt-cli grunt-init bower gulp browserify webpack eslint typings;
