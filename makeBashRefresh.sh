06d0c4e6393ff30dceb968773440146781942b4e
Sy Le <lenguyensy@gmail.com>
2016-10-15 17:27:14 -0700

    Update makeBashRefresh.sh

--- makeBashRefresh.sh
+++ makeBashRefresh.sh
@@ -85,6 +85,7 @@ git config --global alias.b 'branch'
 git config --global alias.branch 'branch -a'
 git config --global alias.p 'push'
 git config --global alias.logs 'log --oneline --decorate'
+git config --global alias.l "log --pretty=format:'%Cred%h%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' --abbrev-commit --date=relative"
 git config --global alias.fap 'fetch --all --prune'
 git config --global alias.st 'status -sb'
 git config --global alias.s 'status -sb'
