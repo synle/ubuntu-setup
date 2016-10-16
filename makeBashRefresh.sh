0aa8dbe32da16fb564e73947bef482e8b3354155
Sy Le <lenguyensy@gmail.com>
2016-10-15 21:06:26 -0700

    Update makeBashRefresh.sh

--- makeBashRefresh.sh
+++ makeBashRefresh.sh
@@ -131,6 +131,8 @@ git config --global alias.stash 'stash --all'
 
 #vim stuffs
 echo "  > Vim & Vundle"
-rm -rf ~/.vim/bundle/Vundle.vim && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null;
+echo "    >> Vim Config .vimrc"
 curl -so- -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/synle/ubuntu-setup/master/vim/.vimrc > ~/.vimrc;
+echo "    >> Install Vundle"
+rm -rf ~/.vim/bundle/Vundle.vim && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null;
 vim +BundleInstall +qall &> /dev/null;
