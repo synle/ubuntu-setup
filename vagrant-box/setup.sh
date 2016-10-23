a6146c1e259c1c9278288ccedcdeebbcb5d39198
Sy Le <lenguyensy@gmail.com>
2016-10-22 22:41:58 -0700

    Update setup.sh

--- vagrant-box/setup.sh
+++ vagrant-box/setup.sh
@@ -45,7 +45,6 @@ echo "Install nvm for syle"
 export NVM_DIR="$HOME_SYLE/.nvm" && (
   git clone https://github.com/creationix/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
 ) && . "$NVM_DIR/nvm.sh"
 
 
