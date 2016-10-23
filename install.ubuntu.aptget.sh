13eda70c28848e1a3b83ab45b0e6e4e0c7d459ad
Sy Le <lenguyensy@gmail.com>
2016-10-22 22:56:16 -0700

    Create install.ubuntu.aptget.sh

---
+++ install.ubuntu.aptget.sh
@@ -0,0 +1,25 @@
+echo "Ubuntu apt-get update...";
+sudo apt-get update -y;  
+
+echo "Ubuntu apt-get install...";
+sudo apt-get install  -y \
+  --fix-missing \
+  git \
+  g++ \
+  unzip \
+  vim \
+  make \
+  mysql-client \
+  openjdk-7-jdk \
+  ant \
+  gradle \
+  maven \
+  # build-essential \
+  # python \
+  # python-dev \
+  # python-software-properties \
+  # software-properties-common \
+  # supervisor \
+  # automake \
+  # gnuplot \
+  curl;
