5daefcd1a8b38277c484f8aa65730e048bf8ff98
Sy Le <lenguyensy@gmail.com>
2016-10-22 22:06:58 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -11,8 +11,7 @@ if [ $is_ubuntu == "1" ]; then
   echo '. ~/.bash_syle' >> $BASH_PATH;
 
   echo "Ubuntu apt-get install...";
-#   sudo apt-get update -y;
-  sudo apt-get install -y git;
+  sudo apt-get update -y;
   sudo apt-get install -y curl git build-essential openjdk-7-jdk python-dev python-software-properties software-properties-common g++ python supervisor automake gnuplot unzip vim ant gradle maven git maven make mysql-client;
 fi
 if [ "$(uname)" == "Darwin" ]; then
