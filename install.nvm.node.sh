b03a2537d0eae11220b4721a444abc5cb714f898
Sy Le <lenguyensy@gmail.com>
2016-10-22 22:54:04 -0700

    Create install.nvm.node.sh

---
+++ install.nvm.node.sh
@@ -0,0 +1,20 @@
+echo "Install node@v0.12.15 - stable - iojs";
+nvm install v0.12.15;
+nvm install stable;
+nvm install iojs;
+nvm alias default v0.12.15;
+ 
+echo "Install npm@2.15.1";
+npm install npm@2.15.1 -g;
+
+#download node npm deps
+echo "Install Global Node Packages";
+npm i -g \
+    grunt-cli \
+    grunt-init \
+    bower \
+    gulp \
+    browserify \
+    webpack \
+    eslint \
+    typings;
