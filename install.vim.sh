105bb2dffb7c4f5656cf5b284b5b81176fd9fff1
Sy Le <lenguyensy@gmail.com>
2017-01-11 09:25:17 -0800

    Create install.vim.sh

---
+++ install.vim.sh
@@ -0,0 +1,6 @@
+echo "   Vim & Vundle"
+echo "      Vim Config .vimrc"
+curl -so- -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/synle/ubuntu-setup/master/vim/.vimrc > ~/.vimrc;
+echo "      Install Vundle"
+rm -rf ~/.vim/bundle/Vundle.vim && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null;
+vim +BundleInstall +qall &> /dev/null;
