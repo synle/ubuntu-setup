fb33514729ebd6c5ed84129aeaee1e8ff923450e
Sy Le <lenguyensy@gmail.com>
2017-01-11 13:45:01 -0800

    Update install.hadoop.sh

--- install.hadoop.sh
+++ install.hadoop.sh
@@ -1,3 +1,13 @@
+#https://www.tutorialspoint.com/hadoop/hadoop_enviornment_setup.htm
+
+#users
+useradd hadoop
+passwd hadoop
+
+ssh-keygen -t rsa 
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys 
+chmod 0600 ~/.ssh/authorized_keys 
+
 #java
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
