3e37daa39a165d98d27d10ccdabdf1c43eab8748
Sy Le <lenguyensy@gmail.com>
2017-01-17 22:26:29 -0800

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -16,16 +16,12 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 
-#source the bash profile
-echo "Install nvm@v0.33.0 if needed"
-[ -s $NVM_DIR/nvm.sh ] || curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | NVM_DIR=$NVM_DIR bash
-
-echo "Re-source Bash Profile for nvm binary"
-. ${BASH_PATH}
+echo "Install nvm if needed (idempotent)"
+[ -s ~/.nvm/nvm.sh ] || curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash -
 
 echo "Install nvm, node. npm and stuffs"
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/install.nvm.node.sh | bash -
 
-echo "refresh"
+echo "Refresh (idempotent)"
 curl -H 'Cache-Control: no-cache' -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/makeBashRefresh.sh | bash -
 
