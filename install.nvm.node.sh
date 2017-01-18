20ce55b9bfee33e25c5d867631f1de38a1c16866
Sy Le <lenguyensy@gmail.com>
2017-01-17 22:05:44 -0800

    Update install.nvm.node.sh

--- install.nvm.node.sh
+++ install.nvm.node.sh
@@ -8,18 +8,24 @@ nvm install v0.12.15;
 nvm install stable;
 nvm install iojs;
 nvm alias default v0.12.15;
+
+
+#resource nvm if needed
+[ -s /opt/nvm/nvm.sh ] && . /opt/nvm/nvm.sh;
+[ -s ~/.nvm/nvm.sh ] && . ~/.nvm/nvm.sh;
+
+nvm use default;
  
 echo "Install npm@2.15.1";
-npm install npm@2.15.1 -g;
+npm i -g npm@2.15.1;
 
 #download node npm deps
 echo "Install Global Node Packages";
-npm i -g \
-    grunt-cli \
-    grunt-init \
-    bower \
-    gulp \
-    browserify \
-    webpack \
-    eslint \
-    typings;
+npm i -g grunt-cli;
+npm i -g grunt-init;
+npm i -g bower;
+npm i -g gulp;
+npm i -g browserify;
+npm i -g webpack;
+npm i -g eslint;
+npm i -g typings;
