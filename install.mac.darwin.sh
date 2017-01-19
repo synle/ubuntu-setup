e416fcc1d1d6432f0757a48f80152b2d6149e6e9
Sy Le <lenguyensy@gmail.com>
2017-01-19 08:53:47 -0800

    Update install.mac.darwin.sh

--- install.mac.darwin.sh
+++ install.mac.darwin.sh
@@ -2,4 +2,10 @@ echo "Install Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Brew install stuffs"
-brew install jq fzf;
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew unlink php56
+brew install jq;
+brew install fzf;
+brew install php70
