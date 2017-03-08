70ef23c8d75ad876eebcf4c588e291a2245df3f3
Sy Le <lenguyensy@gmail.com>
2017-03-08 07:49:59 -0800

    Update bash-alias-cmd.sh

--- bash-alias-cmd.sh
+++ bash-alias-cmd.sh
@@ -143,16 +143,36 @@ function compareGit(){
 }
 
 
-#case insenstive autocomplete
+
+
+# set up inputrc preferences.
+echo """
+# case insenstive autocomplete
+# ignore case for autocomplete' 
+set completion-ignore-case on' 
+
+# https://gist.github.com/gregorynicholas/1812027
+set expand-tilde on' 
+set show-all-if-ambiguous on' 
+set visible-stats on' 
+#set match-hidden-files off' 
+
+# http://hiltmon.com/blog/2013/03/12/better-bash-shell-expansion/
+# shift tab to reverse auto complete.
+"\e[Z": "\e-1\C-i"'   
+""" >  ~/.inputrc
+
+
+# 
 echo "" >  ~/.inputrc
 echo '#ignore case for autocomplete' >> ~/.inputrc
 echo 'set completion-ignore-case on' >> ~/.inputrc
 
-#https://gist.github.com/gregorynicholas/1812027
+# https://gist.github.com/gregorynicholas/1812027
 echo 'set expand-tilde on' >> ~/.inputrc
 echo 'set show-all-if-ambiguous on' >> ~/.inputrc
 echo 'set visible-stats on' >> ~/.inputrc
 echo '#set match-hidden-files off' >> ~/.inputrc
 
-#http://hiltmon.com/blog/2013/03/12/better-bash-shell-expansion/
+# http://hiltmon.com/blog/2013/03/12/better-bash-shell-expansion/
 echo '"\e[Z": "\e-1\C-i"'  >> ~/.inputrc # shift tab to reverse auto complete.
