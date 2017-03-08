43ef4070070a7a38e7b9160d0e5b6f4690844582
Sy Le <lenguyensy@gmail.com>
2017-03-08 10:10:06 -0800

    Update install.nvm.node.sh

--- install.nvm.node.sh
+++ install.nvm.node.sh
@@ -1,7 +1,3 @@
-function installNodeModulesIfNeeded(){
-    type $@ &> /dev/null || (echo "NPM Install $@" && npm i -g $@)
-}
-
 #resource nvm if needed
 [ -s /opt/nvm/nvm.sh ] && . /opt/nvm/nvm.sh;
 [ -s ~/.nvm/nvm.sh ] && . ~/.nvm/nvm.sh;
@@ -17,14 +13,3 @@ nvm use default;
  
 echo "Install npm@2.15.1";
 npm i -g npm@2.15.1;
-
-#download node npm deps
-echo "Install Global Node Packages";
-npm i -g grunt-cli;
-installNodeModulesIfNeeded  grunt-init;
-installNodeModulesIfNeeded bower;
-installNodeModulesIfNeeded gulp;
-installNodeModulesIfNeeded browserify;
-installNodeModulesIfNeeded webpack;
-installNodeModulesIfNeeded eslint;
-installNodeModulesIfNeeded typings;
