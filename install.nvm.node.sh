8e3aec187525eea13447bc3e3f75850babcecfa3
Sy Le <lenguyensy@gmail.com>
2017-03-12 16:30:48 -0700

    Update install.nvm.node.sh

--- install.nvm.node.sh
+++ install.nvm.node.sh
@@ -11,9 +11,8 @@ function installNvmNodeVersionIfNeeded(){
     fi
 }
 
-echo "  git clone nvm"
-[ -d $NVM_BASE_PATH ] || (echo "    INSTALL nvm" && curl -so- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash -  &> /dev/null)
-[ -d $NVM_BASE_PATH ] && echo "    SKIP nvm"
+[ -d $NVM_BASE_PATH ] || (echo "  git clone nvm" && curl -so- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash -  &> /dev/null)
+[ -d $NVM_BASE_PATH ] && echo "  SKIP git clone nvm"
 type nvm &> /dev/null || . "$NVM_BASE_PATH/nvm.sh"
 
 echo "  nvm install"
