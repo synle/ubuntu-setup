068b0c9eae82811bf2d2fea9de3cbd0850b4011c
Sy Le <lenguyensy@gmail.com>
2017-03-12 16:23:39 -0700

    Update install.mac.darwin.sh

--- install.mac.darwin.sh
+++ install.mac.darwin.sh
@@ -8,26 +8,31 @@ is_os_darwin_mac=0
 #################################
 
 function installMacAppIfNotThereAlready(){
-    type $@ &> /dev/null || (echo "  brew install $@" && brew install $@);
-    type $@ &> /dev/null && echo "  SKIPPED brew install $@"
+    type $@ &> /dev/null || (echo "    INSTALL $@" && brew install $@);
+    type $@ &> /dev/null && (echo "    SKIP    $@")
 }
 
 if [ $is_os_darwin_mac == "1" ]
 then
-    echo "Install Homebrew"
+    echo "setting up homebrew"
     hasHomebrewInstalled=1
     type brew &> /dev/null || hasHomebrewInstalled=0
+    type brew &> /dev/null && echo "  SKIP homebrew"
     
     if [ $hasHomebrewInstalled == "0" ]
     then
+        echo "  INSTALL homebrew"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        echo "Brew install stuffs"
+        
+        echo "  brew tap"
         brew tap homebrew/dupes
         brew tap homebrew/versions
         brew tap homebrew/homebrew-php
         #     brew unlink php56
-        brew install php70;
+        #     brew install php70;
     fi
+    
+    echo "  brew install"
     installMacAppIfNotThereAlready jq;
     installMacAppIfNotThereAlready fzf;
     installMacAppIfNotThereAlready tig;
