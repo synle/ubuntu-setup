df6030bd10df071fa4f17069dc681dd2e1ab89b6
Sy Le <lenguyensy@gmail.com>
2017-03-12 16:21:05 -0700

    Update install.nvm.node.sh

--- install.nvm.node.sh
+++ install.nvm.node.sh
@@ -4,19 +4,19 @@ NVM_BASE_PATH=~/.nvm
 function installNvmNodeVersionIfNeeded(){
     if [ ! -d "$NVM_BASE_PATH/versions/node/$@" ]
     then
-        echo "    nvm install $@"
+        echo "    INSTALL $@"
         nvm install $@ &> /dev/null
     else
-        echo "    SKIPPED nvm install $@"
+        echo "    SKIP    $@"
     fi
 }
 
-echo "  Install nvm"
+echo "  setting up nvm"
 [ -d $NVM_BASE_PATH ] || (echo "    Install NVM" && curl -so- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash -  &> /dev/null)
 [ -d $NVM_BASE_PATH ] && echo "    SKIPPED Install NVM"
 type nvm &> /dev/null || . "$NVM_BASE_PATH/nvm.sh"
 
-echo "  Install Nodes"
+echo "  nvm install"
 installNvmNodeVersionIfNeeded v6.10.0
 
 if [ ! -d "$NVM_BASE_PATH/versions/node/v0.12.15" ]
