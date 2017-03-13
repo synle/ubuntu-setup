9f5faee5cdabc5b85c18a96e858ab14e62431bfa
Sy Le <lenguyensy@gmail.com>
2017-03-12 17:43:13 -0700

    Update makeWindowInstall.cmd

--- makeWindowInstall.cmd
+++ makeWindowInstall.cmd
@@ -1,1 +1,9 @@
 setx path "%path%;C:\Program Files\Sublime Text 3;"
+
+http://stackoverflow.com/questions/2710748/run-curl-commands-from-windows-console
+powershell -Command "(new-object net.webclient).DownloadString('http://example.com')"
+
+hyper term config
+curl https://raw.githubusercontent.com/synle/ubuntu-setup/master/windows/hyperterm.js > ~/.hyper.js
+
+wget https://github.com/xilun/cbwin/archive/master.zip
