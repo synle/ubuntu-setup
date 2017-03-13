13fc83ab90e53b827e2064243b3a731b38badebd
Sy Le <lenguyensy@gmail.com>
2017-03-12 18:06:12 -0700

    Update makeWindowInstall.cmd

--- makeWindowInstall.cmd
+++ makeWindowInstall.cmd
@@ -1,6 +1,1 @@
-setx path "%path%;C:\Program Files\Sublime Text 3;"
-
-http://stackoverflow.com/questions/2710748/run-curl-commands-from-windows-console
-powershell -Command "(new-object net.webclient).DownloadString('http://example.com')"
-
-wget https://github.com/xilun/cbwin/archive/master.zip
+setx path "%path%;C:\Program Files\Sublime Text 3;C:\cbwin"
