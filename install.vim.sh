38e29c80a40fe483a00b58058decfdac202ca88b
Sy Le <lenguyensy@gmail.com>
2017-03-22 17:57:29 -0700

    Update install.vim.sh

--- install.vim.sh
+++ install.vim.sh
@@ -3,7 +3,7 @@ echo "  Vim Config .vimrc"
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/misc/vimrc > ~/.vimrc
 echo "  Install Vundle"
 [ -d ~/.vim/bundle/Vundle.vim ] \
-    || git clone https://github.com/gmarik/Vundle.vim.git \
+    || git clone --depth 1 -b master https://github.com/gmarik/Vundle.vim.git \
     ~/.vim/bundle/Vundle.vim &> /dev/null
 echo "    Install Vundle Plugins"
 vim +BundleInstall +qall &> /dev/null
