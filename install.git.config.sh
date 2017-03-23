6d61db9bee26ee0aefe789205f749ddf44db480f
Sy Le <lenguyensy@gmail.com>
2017-03-23 16:57:33 -0700

    Update install.git.config.sh

--- install.git.config.sh
+++ install.git.config.sh
@@ -15,7 +15,7 @@ git config --global merge.prompt false
 
 #git alias
 echo "    Git Aliases"
-git config --global alias.del-merged "!git branch --merged | grep -v '*' | xargs git branch -d"
+git config --global alias.del-merged "!git fetch --all --prune || !git branch --merged | grep -v '*' | xargs git branch -d"
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.co 'checkout'
 git config --global alias.cob 'checkout -b'
