0290dc669750811a6e2b128d792fdfe88688aae4
Sy Le <lenguyensy@gmail.com>
2017-04-05 17:28:39 -0700

    Update bash-util-ubuntu-wsl.sh

--- bash-util-ubuntu-wsl.sh
+++ bash-util-ubuntu-wsl.sh
@@ -1,6 +1,6 @@
 # alias subl="wstart subl"
 
 # for work
-alias force=/mnt/c/force-cli/force.exe
-alias subl=subl.exe
-alias pbcopy=win32yank.exe -i
+alias force="force.exe"
+alias subl="subl.exe"
+alias pbcopy="win32yank.exe -i"
