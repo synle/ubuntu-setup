d5c508524217c04172909cee3d5d8410ae6af844
Sy Le <lenguyensy@gmail.com>
2017-04-07 12:19:31 -0700

    Update bash-alias-cmd.sh

--- bash-alias-cmd.sh
+++ bash-alias-cmd.sh
@@ -440,7 +440,8 @@ function compileSfdcAuraQuick(){
     export FZF_DEFAULT_COMMAND='
       (git ls-tree -r --name-only HEAD ||
        find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
-          sed s/^..//) 2> /dev/null'
+          sed s/^..//) 2> /dev/null |\
+          filterUnwanted'
 
 
 ############################################
