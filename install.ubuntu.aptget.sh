8ba44551f9e1884a79c61d05343716990d793a34
Sy Le <lenguyensy@gmail.com>
2017-04-08 08:36:16 -0700

    Update install.ubuntu.aptget.sh

--- install.ubuntu.aptget.sh
+++ install.ubuntu.aptget.sh
@@ -33,16 +33,19 @@ then
     installAptGetModuleIfNeeded figlet
     installAptGetModuleIfNeeded autoconf
     installAptGetModuleIfNeeded automake
-    # sudo apt-get install  -y --fix-missing g++
+    installAptGetModuleIfNeeded gcc
+    installAptGetModuleIfNeeded g++
+    installAptGetModuleIfNeeded build-essential
+    installAptGetModuleIfNeeded unzip
+    
+    # java stuffs
+    installAptGetModuleIfNeeded openjdk-7-jdk
+    installAptGetModuleIfNeeded ant
+    installAptGetModuleIfNeeded gradle
+    installAptGetModuleIfNeeded maven
+    
     # sudo apt-get install  -y --fix-missing mysql-client
-    # sudo apt-get install  -y --fix-missing openjdk-8-jdk
-    # sudo apt-get install  -y --fix-missing ant
-    # sudo apt-get install  -y --fix-missing gradle
-    # sudo apt-get install  -y --fix-missing maven
     #
-    # sudo apt-get install  -y --fix-missing unzip
-    # sudo apt-get install  -y --fix-missing make
-    # sudo apt-get install  -y --fix-missing build-essential
     # sudo apt-get install  -y --fix-missing python-dev
     # sudo apt-get install  -y --fix-missing python-software-properties
     # sudo apt-get install  -y --fix-missing software-properties-common
