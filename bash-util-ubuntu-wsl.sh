f268cdb0c573119f27a0511b8d8c3b5df641dbf5
Sy Le <lenguyensy@gmail.com>
2017-04-08 11:26:10 -0700

    Update bash-util-ubuntu-wsl.sh

--- bash-util-ubuntu-wsl.sh
+++ bash-util-ubuntu-wsl.sh
@@ -3,3 +3,7 @@ alias subl="subl.exe"
 alias pbcopy="win32yank.exe -i"
 alias pbpaste="win32yank.exe -o"
 alias open="explorer.exe"
+
+# for android.
+alias adb="adb.exe"
+alias fastboot="fastboot.exe"
