3330328f03140d4d475cfc7f34444e84bf6aca7a
Sy Le <lenguyensy@gmail.com>
2017-04-14 13:39:20 -0700

    Update makeBashInstall.sh

--- makeBashInstall.sh
+++ makeBashInstall.sh
@@ -103,6 +103,7 @@ else
     mkdir -p /mnt/c/opt
     echo  "        Win32Yank (pbcopy & pbpaste)"
     [ -s /mnt/c/opt/win32yank.exe ] || curl https://raw.githubusercontent.com/synle/ubuntu-setup/master/windows/binary/win32yank.exe -o /mnt/c/opt/win32yank.exe
+    sudo ln -f -s /mnt/c/opt/win32yank.exe /usr/local/bin/win32yank
     echo  "        force-cli (Salesforce)"
     [ -s /mnt/c/opt/force.exe ]     || curl https://s3-us-west-2.amazonaws.com/force-cli/heroku/force/v0.22.67/windows-amd64/force.exe -o /mnt/c/opt/force.exe
     sudo ln -f -s /mnt/c/opt/force.exe /usr/local/bin/force
