2c9760fbba8a0b28c45a28f436d270cb88282182
Sy Le <lenguyensy@gmail.com>
2017-06-27 21:04:23 -0700

    Update install.npm.node.sh

--- install.npm.node.sh
+++ install.npm.node.sh
@@ -4,21 +4,15 @@ NVM_BASE_PATH=~/.nvm
 # resource nvm
 . "$NVM_BASE_PATH/nvm.sh"
 
-function installNodeModulesIfNeeded(){
-    #type $@ &> /dev/null || (echo "    INSTALL $@" && npm i -g $@)
-    #type $@ &> /dev/null &&  (echo "    SKIP    $@")
-    echo "    INSTALL $@" && npm i -g $@
-}
-
 #download node npm deps
 echo "  npm i -g";
-# npm i -g grunt-cli;
-installNodeModulesIfNeeded grunt-init;
-installNodeModulesIfNeeded bower;
-installNodeModulesIfNeeded gulp;
-installNodeModulesIfNeeded browserify;
-installNodeModulesIfNeeded webpack;
-installNodeModulesIfNeeded eslint;
-installNodeModulesIfNeeded typings;
-installNodeModulesIfNeeded less;
-installNodeModulesIfNeeded create-react-app;
+npm i -g \
+    grunt-init \
+    bower \
+    gulp \
+    browserify \
+    webpack \
+    eslint \
+    typings \
+    less \
+    create-react-app
