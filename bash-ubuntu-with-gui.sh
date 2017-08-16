acd72cfe59ff9afce2d15210822ae5afe90a5ff5
Sy Le <lenguyensy@gmail.com>
2017-08-16 07:10:20 -0700

    Update bash-ubuntu-with-gui.sh

--- bash-ubuntu-with-gui.sh
+++ bash-ubuntu-with-gui.sh
@@ -1,3 +1,7 @@
 # applied to only ubuntu with gui (xubuntu)
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+
+
+# synaptic mouse touchpad settings
+synclient MaxTapTime=0
