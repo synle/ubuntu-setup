f5c01000e77f968ba06df0e8a7c89626e7097a3e
Sy Le <lenguyensy@gmail.com>
2017-10-08 09:00:46 -0700

    Update install.mac.darwin.sh

--- install.mac.darwin.sh
+++ install.mac.darwin.sh
@@ -104,7 +104,9 @@ then
     # showing the Dock
     defaults write com.apple.dock autohide-time-modifier -float 0;
     defaults write com.apple.dock autohide-delay -float 0;
-
+    
+    # restart it...
+    killall Dock
 
     #screenshot folder
     mkdir -p ~/Desktop/_screenshots
