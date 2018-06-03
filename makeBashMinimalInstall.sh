ef7c09c2a7b045e9c434d52158330b09b1097078
Sy Le <lenguyensy@gmail.com>
2018-06-03 10:41:26 -0700

    Update makeBashMinimalInstall.sh

--- makeBashMinimalInstall.sh
+++ makeBashMinimalInstall.sh
@@ -1,6 +1,15 @@
 BASH_SYLE=~/.bash_syle
 
 echo '# begin syle bash' > $BASH_SYLE
+
+# bash completion
+echoo "Bash Completions"
+echo  "  Git Completion"
+curl -so- https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> $BASH_SYLE
+echo  "  NPM Completion"
+type npm &> /dev/null  && npm set progress=false && npm completion >> $BASH_SYLE
+
+
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/bash-alias-cmd.sh >> $BASH_SYLE
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/bash-prompt.sh >> $BASH_SYLE
 curl -so- https://raw.githubusercontent.com/synle/ubuntu-setup/master/install.git.config.sh | bash -
