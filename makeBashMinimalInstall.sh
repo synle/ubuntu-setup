ecd7cf3c88f3a2485999be04e0284cb637ce6568
Sy Le <lenguyensy@gmail.com>
2018-08-28 18:37:29 -0700

    Update makeBashMinimalInstall.sh

--- makeBashMinimalInstall.sh
+++ makeBashMinimalInstall.sh
@@ -26,28 +26,10 @@ function echoo(){ printf "\e[1;31m$@\n\e[0m"; }
 # end prep work #
 ##################################################################
 
-
-# bootstrap if needed
-echoo "Install .bash_syle if needed"
-grep -q -F '.bash_syle' $BASH_PATH || echo  """
-# syle bash
-[ -s $BASH_SYLE ] && . $BASH_SYLE
-""" >> $BASH_PATH
-
-
-### start bash-syle
-echo  "#!/bin/bash" >> $TEMP_BASH_SYLE
-echo '# begin syle bash' > $TEMP_BASH_SYLE
-
-# bash completion
-echo  "# Bash Completion - git" >> $TEMP_BASH_SYLE
-curlNoCache https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> $TEMP_BASH_SYLE
-echo  "# Bash Completion - npm" >> $TEMP_BASH_SYLE
-type npm &> /dev/null  && npm set progress=false && npm completion >> $TEMP_BASH_SYLE
-
-echo "# Bash Prompt"
-curlNoCache https://raw.githubusercontent.com/synle/ubuntu-setup/master/bash-alias-cmd.sh >> $TEMP_BASH_SYLE
-curlNoCache https://raw.githubusercontent.com/synle/ubuntu-setup/master/bash-prompt.sh >> $TEMP_BASH_SYLE
+##################################################################
+#### Core bash prompt ####
+##################################################################
+curlNoCache https://raw.githubusercontent.com/synle/ubuntu-setup/master/makeBashPromptMinimal.sh | bash -
 
 
 ##################################################################
@@ -58,11 +40,7 @@ echo "nvm & node & npm modules"
 NVM_BASE_PATH=~/.nvm
 curlNoCache https://raw.githubusercontent.com/synle/ubuntu-setup/master/install.nvm.node.sh | bash -
 curlNoCache https://raw.githubusercontent.com/synle/ubuntu-setup/master/install.npm.node.sh | bash -
-echo  '''
-# nvm - node version manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-''' >> $TEMP_BASH_SYLE
+
 
 
 # make-component
@@ -71,23 +49,12 @@ UTIL_MAKE_COMPONENT_PATH=~/synle-make-component
 rm -rf $UTIL_MAKE_COMPONENT_PATH && git clone --depth 1 -b master https://github.com/synle/make-component.git $UTIL_MAKE_COMPONENT_PATH &> /dev/null
 pushd $UTIL_MAKE_COMPONENT_PATH 
 npm i && npm run build
-echo """
-# synle make component
-PATH=\$PATH:$UTIL_MAKE_COMPONENT_PATH
-[ -s '$UTIL_MAKE_COMPONENT_PATH/setup.sh' ] && . '$UTIL_MAKE_COMPONENT_PATH/setup.sh'
-""" >> $TEMP_BASH_SYLE
 popd
 
 
 # fzf (fuzzy find)
 echo "fzf installation"
 rm -rf ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf  &> /dev/null
-echo  """
-# fzf - fuzzy find
-[ -f ~/.fzf.bash ] && . ~/.fzf.bash
-""" >> $TEMP_BASH_SYLE
-
-
 
 # tmux stuffs
 # http://www.hamvocke.com/blog/a-guide-to-customizing-your-tmux-conf/
