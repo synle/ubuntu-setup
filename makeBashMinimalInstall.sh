53df4b1da3ff226e72f06d32ab86f3179ebd6368
Sy Le <lenguyensy@gmail.com>
2018-08-30 11:53:26 -0700

    Update makeBashMinimalInstall.sh

--- makeBashMinimalInstall.sh
+++ makeBashMinimalInstall.sh
@@ -411,12 +411,23 @@ echo 'terminator config'
 
 
 ######################################################################
-# diff-so-fancy
+# diff-so-fancy and integration with git
 ######################################################################
 curlNoCache https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > /tmp/diff-so-fancy
 chmod +x /tmp/diff-so-fancy
 sudo mv /tmp/diff-so-fancy /usr/bin/
-
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
 
 
 
