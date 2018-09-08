50971e12b1d563c97210cab13192993c92e054b0
Sy Le <lenguyensy@gmail.com>
2018-09-08 12:08:40 -0700

    Update makeBashMinimalInstall.sh

--- makeBashMinimalInstall.sh
+++ makeBashMinimalInstall.sh
@@ -206,10 +206,10 @@ git config --global color.diff.old        "red bold"
 git config --global color.diff.new        "green bold"
 git config --global color.diff.whitespace "red reverse"
 # complex commands
-git config --global alias.del-merged         \"!git branch --merged | grep -v '*' | xargs git branch -d\"
-git config --global alias.del-merged-staging \"!git branch --merged origin/staging | grep -v '*' | xargs git branch -d\"
-git config --global alias.graph \"log --all --graph --pretty=format:'%Cred%h%Creset%C(auto)%d%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' --abbrev-commit --date=relative\"
-git config --global alias.l \"log --pretty=format:'%Cred%h%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' --abbrev-commit --date=relative\"
+## git config --global alias.del-merged         \"!git branch --merged | grep -v '*' | xargs git branch -d\"
+## git config --global alias.del-merged-staging \"!git branch --merged origin/staging | grep -v '*' | xargs git branch -d\"
+## git config --global alias.graph \"log --all --graph --pretty=format:'%Cred%h%Creset%C(auto)%d%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' --abbrev-commit --date=relative\"
+## git config --global alias.l \"log --pretty=format:'%Cred%h%Creset %s %Cgreen%cr %C(bold blue)%an%Creset' --abbrev-commit --date=relative\"
 """ >> $TEMP_BASH_SYLE
 
 echo """
